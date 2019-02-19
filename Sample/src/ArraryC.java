caf8052468a5137dd9dbd18504da6e968abd6572
ChandanaKlcu <chandanaklcu01@gmail.com>
2019-02-19 13:50:38 +0530

    Softvision BTATAssessment

--- Sample/src/ArraryC.java
+++ Sample/src/ArraryC.java
@@ -3,10 +3,7 @@ public class ArraryC
 {
 	public static void main(String[] args) 
 	{
-		List<String> arr = new ArrayList<String>();
-		arr.add("Love yourself");
-		arr.add("Alive is Awesome"); 
-	    arr.add("Be in present"); 
-	System.out.println("the list is"+arr);
+	
+	System.out.println("Hello World");
 	}
 }
